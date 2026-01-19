26c048489931995978674846feb3f038b2f22365
Pablo G <mtesqtsdlc2@gmail.com>
2026-01-19 19:50:06 +0100

    [main] : Add XML documentation to ApplicationDynamoContext
    ━━━━━━━━━━━━━━━━━━━━━━━━━
    
    Who would've guessed that wrapping `IDynamoDBContext` in descriptive XML summaries was the missing feature of the year?

--- VibraHeka/src/Infrastructure/Persistence/ApplicationDynamoContext.cs
+++ VibraHeka/src/Infrastructure/Persistence/ApplicationDynamoContext.cs
@@ -3,7 +3,14 @@
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 
+/// <summary>
+/// Provides a wrapper for the Amazon DynamoDB client and context, enabling interaction with the DynamoDB tables.
+/// </summary>
 public class ApplicationDynamoContext(IAmazonDynamoDB client)
 {
+    /// <summary>
+    /// Gets the instance of <see cref="IDynamoDBContext"/> which is used to interact with the Amazon DynamoDB database.
+    /// This property provides a high-level abstraction for accessing tables, executing queries, and performing database operations.
+    /// </summary>
     public IDynamoDBContext Context { get; } = new DynamoDBContextBuilder().WithDynamoDBClient(() => client).Build();
 }
