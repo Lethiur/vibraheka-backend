3dadf3212beb3a10f3e87e7db3dbb12834413484
Pablo G <mtesqtsdlc2@gmail.com>
2026-02-07 20:17:07 +0100

    [VH-010_Therapy_calendar]: Added S3 bucket for user material with public read access. Because who needs privacy when you have JSON policies?
    ───────────────────────────────────────────────────────────────────
    
    - Spun up a new S3 bucket (`VH_user_material`) to store user material. Fancy name, I know.
    - Public read access enabled for all bucket objects, because sharing is caring (and apparently mandatory).
    - Enforced ownership controls with "BucketOwnerEnforced," because clearly, we need a babysitter for our buckets.
    - Tweaked public access blocks like a true Terraform wizard, yet somehow found loopholes for public bucket policies.

---
+++ src/Infrastructure/terraform/Users/S3-UsersMaterial.tf
@@ -0,0 +1,41 @@
+﻿resource "aws_s3_bucket" "VH_user_material" {
+  bucket = "vibraheka-user-material-${terraform.workspace}"
+}
+
+resource "aws_s3_bucket_public_access_block" "VH_user_material_access" {
+  bucket = aws_s3_bucket.VH_user_material.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+
+  # Necesario para permitir una bucket policy pública
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "VH_user_material_ownership" {
+  bucket = aws_s3_bucket.VH_user_material.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_policy" "VH_user_material_public_read_objects" {
+  bucket = aws_s3_bucket.VH_user_material.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadOnlySpecificPrefix"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+
+        # Lectura pública de TODOS los objetos del bucket
+        Resource  = "${aws_s3_bucket.VH_user_material.arn}/*"
+      }
+    ]
+  })
+}
