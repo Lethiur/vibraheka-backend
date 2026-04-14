753ae149ca3673b44a5170c0d8bd0f03379cf737
Pablo Galdeano <mtesqtsdlc2@gmail.com>
2026-04-14 22:35:01 +0200

    Merge pull request #29
    * VH-68 Prod environment and preparing for videos - updating Terraform
    
    * fix: Removing redundant executions of flow

---
+++ src/Domain/Recordings/Ports/Out/RecordingStoragePort.cs
@@ -0,0 +1,6 @@
+namespace VibraHeka.Domain.Recordings.Ports.Out;
+
+public class RecordingStoragePort
+{
+    
+}
--- src/Infrastructure/terraform/Lambdas/Payments/terraform/event-bridge.tf
+++ src/Infrastructure/terraform/Lambdas/Payments/terraform/event-bridge.tf
@@ -22,7 +22,7 @@ resource "aws_cloudwatch_event_rule" "stripe_rule" {
 
 resource "aws_cloudwatch_event_target" "stripe_lambda_target" {
   rule      = aws_cloudwatch_event_rule.stripe_rule.name
-  event_bus_name = "aws.partner/stripe.com/ed_test_61U8OhWNACpdhq6GG16U8LJp90CQBOsqL618O7RCiVVA"
+  event_bus_name = terraform.workspace == "main" ? "aws.partner/stripe.com/ed_61UVaMeHyCfrgJPlT16PJ3we90CQ4242m36qvV5kGV0y" : "aws.partner/stripe.com/ed_test_61U8OhWNACpdhq6GG16U8LJp90CQBOsqL618O7RCiVVA"
   target_id = "stripeLambda"
   arn       = aws_lambda_function.stripe_lambda.arn
 }
